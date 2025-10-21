65b73da1ba2066497bfb5f202a80e8ddd146ec8e
Amp&rsand <113919662+MuriloSchneiderS@users.noreply.github.com>
2025-10-20 21:52:28 -0300

    Update MotoTest.java

--- teste-de-software/exerciciosJunit/src/test/java/MotoTest.java
+++ teste-de-software/exerciciosJunit/src/test/java/MotoTest.java
@@ -1,5 +1,7 @@
 import com.mycompany.exerciciosjunit.Moto;
 import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MotoTest {
@@ -15,12 +17,59 @@ public class MotoTest {
     }
     @BeforeEach
     public void fazAntesCada() {
-        moto = new Moto("motoTeste");
+        moto = new Moto("Yamaha");
         System.out.println("===inicio novo teste===");
     }
     @AfterEach
     public void fazDepoisCada() {
         System.out.println("===fim teste===");
+    }
+    
+    @Test
+    @DisplayName("Teste do metodo acelera()")
+    @Order(0)
+    void testaAceleraEFreia() {
+        //Acelera até 50
+        moto.acelera(50);
+        double velocidade50 = moto.getVelocidade();
+        
+        //Acelera até 100
+        moto.acelera(50);
+        double velocidade100 = moto.getVelocidade();
+        
+        //Freia para 70
+        moto.freia(30);
+        double velocidade70 = moto.getVelocidade();
+        
+        //Freia para 0
+        moto.freia(100);
+        double velocidade0 = moto.getVelocidade();
+        
+        Assertions.assertAll(
+                () -> assertEquals(50.0, velocidade50),
+                () -> assertEquals(100.0, velocidade100),
+                () -> assertEquals(70.0, velocidade70),
+                () -> assertEquals(0.0, velocidade0)
+        );
+    }
+
+    @Test
+    @DisplayName("Teste Acelera com velocidade negativa.")
+    @Order(1)
+    void testaAceleraComVelocidadeNegativa() {
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            moto.acelera(-10);
+        });//Guarda mensagem de erro disparada ao acelerar com valor negativo
+        assertEquals("vel deve ser positiva", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Teste Freia mais que velocidade atual")
+    @Order(2)
+    void testaFreiaMaisQueVelocidadeAtual() {
+        moto.acelera(30);
+        moto.freia(40);//Tenta freiar mais do que a velocidade atual
+        assertEquals(0.0, moto.getVelocidade(), 0.01);//Deve parar
     }
 }
 /*
